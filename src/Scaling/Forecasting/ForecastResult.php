452bb314eb49ed070c708c0e4054ba919ed08a14
Sylvester Damgaard <sn@cbox.dk>
2026-04-16 19:33:46 +0200

    feat(v2): add ForecastResult DTO

--- src/Scaling/Forecasting/ForecastResult.php
+++ src/Scaling/Forecasting/ForecastResult.php
@@ -4,4 +4,18 @@ declare(strict_types=1);
 
 namespace Cbox\LaravelQueueAutoscale\Scaling\Forecasting;
 
-final readonly class ForecastResult {}
+final readonly class ForecastResult
+{
+    public function __construct(
+        public float $projectedRate,
+        public float $rSquared,
+        public float $slope,
+        public int $sampleCount,
+        public bool $hasSufficientData,
+    ) {}
+
+    public static function insufficientData(): self
+    {
+        return new self(0.0, 0.0, 0.0, 0, false);
+    }
+}
